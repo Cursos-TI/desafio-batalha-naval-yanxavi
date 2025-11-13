3664545a09b440d752742f0708644ad58ba91da5
yan xavier <129429365+yanxavi@users.noreply.github.com>
2025-11-13 17:41:22 +0000

    FIM NÍVEL AVENTUREIRO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,17 +13,10 @@ int main() {
 
     char linhaTabela[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K'};
     int tabuleiro[10][10] = {0};
-    int primeNavio[3] = {3};
-    int segunNavio[3] = {3};
+
+    // LIMITADORES DOS NAVIOS NA DIAGONAL
+   
     
-    tabuleiro[2][3] = 3;
-    tabuleiro[2][4] = 3;
-    tabuleiro[2][5] = 3;
-
-    tabuleiro[6][8] = 3;
-    tabuleiro[7][8] = 3;
-    tabuleiro[8][8] = 3;
-
     printf("TABULEIRO DE BATALHA NAVAL\n\n");
 
     printf("   ");
@@ -33,20 +26,54 @@ int main() {
 
     printf("\n");
 
-    for( int i = 0; i < 10; i++) { // i é a coluna
-// ##### IMPRESSÃO DO VALOR DA COLUNA
+    for( int i = 0; i < 10; i++) { // i é a linha
+// ##### IMPRESSÃO DO VALOR DA LINHA
         if (i < 9){ 
               printf("%d |", i + 1);
         }  else { // QUANDO i CHEGA A 9, O TERMINAL IMPRIME SEM O ESPAÇO ENTRE LETRA E O pipe ( | )
             printf("%d|", i + 1); 
         };
+        int limitador1, limitador2; //LIMITADORES PARA OS NAVIOS DA DIAGONAL PRINCIPAL E EM OUTRA DIAGONAL 
 
 
 // ####  IMPRESSÃO DAS LINHAS
         for(int j = 0; j < 10; j++){ // j é a linha
-            printf("%d  ", tabuleiro[i][j]);
+            limitador1 = j; // O LIMITADOR VAI TER OS MESMOS VALORES DE j, limitador1 = x ↔ j = x;
+            limitador2 = j; // O LIMITADOR VAI TER OS MESMOS VALORES DE j, limitador2 = x ↔ j = x;
 
-            if (j > 9) break;
+            // IMPRESSÃO NA DIAGONAL PRINCIPAL DE E5 ATÉ G7 #####################################################################################################
+            if (j == i){ // EXECUTADO QUANDO j E i FOREM IGUAIS
+
+                
+                if (limitador1 < 7){ // EXECUTADO SOMENTE QUANDO limitador1 É MENOR QUE 7, OU SEJA, TERMINA NA COLUNA 'G'
+
+                    // O NAVIO (Nº 3) É COLOCADO NA DIAGONAL A PARTIR DA COLUNA 'E'
+                tabuleiro[i][j] = (limitador1 > 3 ? 3 : 0); // QUANDO limitador1 FOR MAIOR QUE 3, O TABULEIRO RECEBE 3, SE NÃO, RECEBE 0 
+                }
+            } 
+
+            // IMPRESSÃO NA DIAGONAL DE E2 ATÉ C4 #####################################################################################################
+            else if (i + j == 5){
+                if (limitador2 < 5){
+                    tabuleiro[i][j] = limitador2 > 1 ? 3 : 0;
+                };
+            }
+
+            // IMPRESSÃO NA HORIZONTAL DE B8 ATÉ D8 #####################################################################################################
+            else if (i == 7 && j > 0 && j < 4)
+            {
+                tabuleiro[i][j] = 3;
+            
+            }
+            
+            // IMPRESSÃO NA VERICAL J4 ATÉ J6 #####################################################################################################
+            else if ( j == 8 && i > 2 && i < 6)
+            {
+                tabuleiro[i][j] = 3;
+            };
+            
+            printf("%d  ", tabuleiro[i][j]); // PRINTA O VALOR DO TABULEIRO
+          if (j > 9) break;
         };
         
         printf("\n");
